aad484b3565bc2c6347358e46b74a775189b3ed9
65160341 <65160341@go.buu.ac.th>
2024-10-08 14:02:22 +0700

    feat(cems) : create filter and table template

--- CEMS-Client/src/router/index.ts
+++ CEMS-Client/src/router/index.ts
@@ -1,14 +1,31 @@
 import { createWebHistory, createRouter } from 'vue-router'
 import Icon from '../components/template/icon.vue';
 import Content from '../components/template/content.vue';
+import Navbar from '../components/template/navbar.vue';
+import Filter from '../components/template/Filter.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes : [
     { 
+      path: '/icon', 
+      name: 'icon',
+      component:  Icon
+    },
+    { 
       path: '/', 
-      name: 'home',
+      name: 'content',
       component:  Content
+    },
+    { 
+      path: '/nav', 
+      name: 'nav',
+      component:  Navbar
+    },
+    { 
+      path: '/filter', 
+      name: 'filter',
+      component:  Filter
     },
   ]
 })
