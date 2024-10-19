cded0fc78646b02714fc154ffcdb0169bf209e53
RAHN\Loves <65160320@go.buu.ac.th>
2024-10-19 23:45:04 +0700

    feat : Template Status

---
+++ CEMS-Client/src/components/template/ForUseStatus.vue
@@ -0,0 +1,33 @@
+<script setup lang="ts">
+import StatusBudge from './StatusBudge.vue';
+
+</script>
+
+<template>
+    <div class="grid grid-cols-5 gap-5 m-5">
+        <div>
+            <!-- อนุมัติ -->
+            <StatusBudge :status="'sts-approve'"></StatusBudge>
+        </div>
+        <div>
+            <!-- รออนุมัติ -->
+            <StatusBudge :status="'sts-waiting'"></StatusBudge>
+        </div>
+        <div>
+            <!-- ไม่อนุมัติ -->
+            <StatusBudge :status="'sts-notApproved'"></StatusBudge>
+        </div>
+        <div>
+            <!-- แก้ไข -->
+            <StatusBudge :status="'sts-edit'"></StatusBudge>
+        </div>
+        <div>
+            <!-- แบบร่าง -->
+            <StatusBudge :status="'sts-draft'"></StatusBudge>
+        </div>
+    </div>
+
+
+
+
+</template>
