ebda3f63ce9a3aed0e253d6e1a4f98e21835febb
65160341 <65160341@go.buu.ac.th>
2024-11-20 13:21:21 +0700

    fix(ApiExpense) : แก้ไข format ของ Controllers , DTO , AppContext

--- CEMS-Server/Controllers/ExpenseController.cs
+++ CEMS-Server/Controllers/ExpenseController.cs
@@ -7,7 +7,7 @@ using Microsoft.EntityFrameworkCore;
 namespace CEMS_Server.Controllers;
 
 [ApiController]
-[Route("api/[controller]")]
+[Route("api/expense")]
 public class ExpenseController : ControllerBase
 {
     private readonly CemsContext _context;
@@ -23,38 +23,38 @@ public class ExpenseController : ControllerBase
         return _context.CemsRequisitions.ToList();
     }
 
-    [HttpGet("expense")]
+    [HttpGet("list")]
     public async Task<ActionResult<IEnumerable<ExpenseDto>>> GetExpense()
     {
-        var requisition = await _context.CemsRequisitions
-        .Include(e => e.RqUsr)        
-        .Include(e => e.RqPj) 
-        .Include(e => e.RqRqt) 
-        .Include(e => e.RqVh) 
-        .Select(u => new ExpenseDto
-        {
-            RqId = u.RqId ,
-            RqUsrName = u.RqUsr.UsrFirstName + " " + u.RqUsr.UsrLastName,
-            RqPjName = u.RqPj.PjName,
-            RqRqtName = u.RqRqt.RqtName,
-            RqVhName = u.RqVh.VhVehicle,
-            RqDatePay = u.RqDatePay,
-            RqDateWithdraw = u.RqDateWithdraw,
-            RqCode = u.RqCode,
-            RqEmail = u.RqEmail,
-            RqExpenses = u.RqExpenses,
-            RqLocation = u.RqLocation,
-            RqStartLocation = u.RqStartLocation,
-            RqEndLocation = u.RqEndLocation,
-            RqDistance = u.RqDistance,
-            RqPurpose = u.RqPurpose,
-            RqImage = u.RqImage,
-            RqStatus = u.RqStatus,
-            RqProgress = u.RqProgress,
-            RqReason = u.RqReason,
-
-        })
-        .ToListAsync();
+        var requisition = await _context
+            .CemsRequisitions
+            //.Include(e => e.RqUsr)
+            .Include(e => e.RqPj)
+            .Include(e => e.RqRqt)
+            .Include(e => e.RqVh)
+            .Select(u => new ExpenseDto
+            {
+                RqId = u.RqId,
+                //RqUsrName = u.RqUsr.UsrFirstName + " " + u.RqUsr.UsrLastName,
+                RqPjName = u.RqPj.PjName,
+                RqRqtName = u.RqRqt.RqtName,
+                RqVhName = u.RqVh.VhVehicle,
+                RqDatePay = u.RqDatePay,
+                RqDateWithdraw = u.RqDateWithdraw,
+                RqCode = u.RqCode,
+                RqEmail = u.RqEmail,
+                RqExpenses = u.RqExpenses,
+                RqLocation = u.RqLocation,
+                RqStartLocation = u.RqStartLocation,
+                RqEndLocation = u.RqEndLocation,
+                RqDistance = u.RqDistance,
+                RqPurpose = u.RqPurpose,
+                RqImage = u.RqImage,
+                RqStatus = u.RqStatus,
+                RqProgress = u.RqProgress,
+                RqReason = u.RqReason,
+            })
+            .ToListAsync();
 
         return Ok(requisition);
     }
