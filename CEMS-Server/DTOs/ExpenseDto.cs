ee6a2cc8bd9fb571e4b3f3595975d13ef4fc972a
65160341 <65160341@go.buu.ac.th>
2024-11-21 13:55:53 +0700

    feat(test) : สร้างเส้น api ทดลองการดึงข้อมูล

---
+++ CEMS-Server/DTOs/ExpenseDto.cs
@@ -0,0 +1,43 @@
+namespace CEMS_Server.DTOs
+{
+    public class ExpenseDto
+    {
+        public int RqId { get; set; }
+        public string RqUsrName { get; set; }  = null!;
+
+        public string RqPjName { get; set; }  = null!;
+
+        public string RqRqtName { get; set; }  = null!;
+
+        public string RqVhName { get; set; }  = null!;
+
+        public DateOnly RqDatePay { get; set; }
+
+        public DateOnly RqDateWithdraw { get; set; }
+
+        public string? RqCode { get; set; }
+
+        public string RqInsteadEmail { get; set; } = null!;
+
+        public double RqExpenses { get; set; }
+
+        public string? RqLocation { get; set; }
+
+        public string? RqStartLocation { get; set; }
+
+        public string? RqEndLocation { get; set; }
+
+        public string? RqDistance { get; set; }
+
+        public string? RqPurpose { get; set; }
+
+        public string? RqReason { get; set; }
+
+        public string? RqProof { get; set; }
+
+        public string RqStatus { get; set; } = null!;
+
+        public string RqProgress { get; set; } = null!;
+        
+    }
+}
