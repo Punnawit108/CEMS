6c615f7ec277221b7a819eb8188c64640f85c7cb
65160210 <65160210@go.buu.ac.th>
2024-11-27 12:59:59 +0700

    Revert "fix (paymentList,paymentStore): ดึงapiมาแสดงและแก้path api ในstore"
    This reverts commit 9500ae5ff45900a9c8df7c07a4bc009059afe06a.

--- CEMS-Client/src/store/PaymentStore.ts
+++ CEMS-Client/src/store/PaymentStore.ts
@@ -16,21 +16,21 @@ export const usePayment = defineStore('expense', {
     actions:{
         async getAllPaymentList(){
             try{
-                const result = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/payment/list`)
+                const result = await axios.get(`${import.meta.env.VITE_BASE_URL}/payment/list`)
                 this.expense = result.data;
             }catch (error){
                 console.error("Failed to fetch payment data:", error)
             }
         },async getPaymentById(id:string){
             try{
-                const result = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/payment/${id}`)
+                const result = await axios.get(`${import.meta.env.VITE_BASE_URL}/payment/${id}`)
                 return result.data;
             }catch (error){
                 console.error("Failed to fetch payment data:", error)
             }
         },async getAllPaymentHistory(){
             try{
-                const result = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/payment/History`)
+                const result = await axios.get(`${import.meta.env.VITE_BASE_URL}/payment/History`)
                 this.expense = result.data;
             }catch (error){
                 console.error("Failed to fetch payment data:", error)
