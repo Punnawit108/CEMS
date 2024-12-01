39303cbe385474abef7a775f68624908a4b96065
Punnawit108 <65160108@go.buu.ac.th>
2024-12-01 09:58:42 +0700

    docs : เพิ่ม comment ที่ได้รับบมอบหมาย

--- CEMS-Server/DTOs/RequisitionTypeDTO.cs
+++ CEMS-Server/DTOs/RequisitionTypeDTO.cs
@@ -1,3 +1,9 @@
+/**
+* ชื่อไฟล์: RequisitionTypeDTO.cs
+* คำอธิบาย: ใช้สำหรับติดต่อกับฐานข้อมูลตาราง RequisitionType
+* ชื่อผู้เขียน/แก้ไข: นายปุณณะวิชญ์ เชียนพลแสน
+* วันที่จัดทำ/แก้ไข: 26 พฤศจิกายน 2567
+*/
 namespace CEMS_Server.DTOs
 {
     public class RequisitionTypeDTO
