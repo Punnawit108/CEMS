6601bf066d96ff4f4766908e593551aa12894ef1
65160341 <65160341@go.buu.ac.th>
2024-12-02 20:33:39 +0700

    docs : เพิ่ม Comment ของ Detail

--- CEMS-Client/src/store/detail.ts
+++ CEMS-Client/src/store/detail.ts
@@ -1,3 +1,10 @@
+/*
+* ชื่อไฟล์: detail.ts
+* คำอธิบาย: ไฟล์นี้ใช้สำหรับจัดการข้อมูลจาก Api
+* ชื่อผู้เขียน/แก้ไข: นายพรชัย เพิ่มพูลกิจ, นายพงศธร บุญญามา
+* วันที่จัดทำ/แก้ไข: 22 ตุลาคม 2567
+*/
+
 import axios from "axios";
 import { defineStore } from 'pinia';
 import { Expense } from '../types';
@@ -14,6 +21,14 @@ export const useDetailStore = defineStore('detailExpense', {
         //     this.selectedExpense = expense;
         //     await this.getApprover(this.selectedExpense.rqId)
         // },
+
+        /*
+        * คำอธิบาย: ดึงข้อมูลของผู้อนุมัติใบเบิกค่าใช้จ่าย
+        * Input: rqId
+        * Output: ข้อมูลของผู้อนุมัติใบเบิกค่าใช้จ่าย
+        * ชื่อผู้เขียน/แก้ไข: นายพงศธร บุญญามา
+        * วันที่จัดทำ/แก้ไข: 28 พฤศจิกายน 2567
+        */
         async getApprover(rqId: Number) {
             try {
                 const result = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/approval/progress/${rqId}`);
@@ -23,6 +38,13 @@ export const useDetailStore = defineStore('detailExpense', {
                 console.error("Error fetching approver data:", error);
             }
         },
+        /*
+        * คำอธิบาย: ดึงข้อมูลของใบเบิกค่าใช้จ่าย
+        * Input: rqId
+        * Output: ข้อมูลของใบเบิกค่าใช้จ่าย
+        * ชื่อผู้เขียน/แก้ไข: นายพงศธร บุญญามา
+        * วันที่จัดทำ/แก้ไข: 28 พฤศจิกายน 2567
+        */
         async getRequisition(rqId: Number) {
             try {
                 const result = await axios.get(`${import.meta.env.VITE_BASE_URL}/api/expense/${rqId}`);
