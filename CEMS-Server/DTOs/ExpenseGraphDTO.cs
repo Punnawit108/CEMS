b8af7e580efb8bf551983d500a54d21776b94996
armktn <65160093@go.buu.ac.th>
2024-12-02 10:28:56 +0700

    docs: update comment in DTO

--- CEMS-Server/DTOs/ExpenseGraphDTO.cs
+++ CEMS-Server/DTOs/ExpenseGraphDTO.cs
@@ -1,3 +1,9 @@
+/*
+* ชื่อไฟล์: ExpenseReportDTO.cs
+* คำอธิบาย: ใช้สำหรับจัดเรียงตัวแปรของเส้น api ใบเบิก
+* ชื่อผู้เขียน/แก้ไข: นายธีรวัฒน์ นิระมล
+* วันที่จัดทำ/แก้ไข: 1 ธันวาคม 2567
+*/
 namespace CEMS_Server.DTOs
 {
     public class ExpenseGraphDto
