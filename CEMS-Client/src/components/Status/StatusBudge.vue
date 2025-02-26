54016598ed0685b2f0ca95d49c93f0df34b40319
65160210 <65160210@go.buu.ac.th>
2025-02-26 21:33:15 +0700

    feat(StatusBudge) : DefineAsyncComponent

--- CEMS-Client/src/components/Status/StatusBudge.vue
+++ CEMS-Client/src/components/Status/StatusBudge.vue
@@ -6,51 +6,28 @@
 * วันที่จัดทำ/แก้ไข: 11 พฤศจิกายน 2567
 */
 
-import { defineProps, onMounted, shallowRef } from "vue"; // นำเข้า defineProps เพื่อจัดการพร็อพของคอมโพเนนต์
-import StatusAccept from "./StatusAccept.vue";
-import StatusWaiting from "./StatusWaiting.vue";
-import StatusReject from "./StatusReject.vue";
-import StatusEdit from "./StatusEdit.vue";
-import StatusSketch from "./StatusSketch.vue";
-
+import { defineProps, onMounted, ref, defineAsyncComponent, type Component } from "vue"; // นำเข้า defineProps เพื่อจัดการพร็อพของคอมโพเนนต์
 
 // กำหนดพร็อพที่คอมโพเนนต์นี้รับเข้ามา
 
-const currentComponent = shallowRef();
+const currentComponent = ref<Component | null>(null);
 
 const props = defineProps<{
-    status: String // รับค่า status ซึ่งใช้กำหนดสถานะที่จะถูกแสดง
+    status: string // รับค่า status ซึ่งใช้กำหนดสถานะที่จะถูกแสดง
 }>();
+const componentMap = new Map<string, Component>([
+    ['sts-accept', defineAsyncComponent(() => import("./StatusAccept.vue"))],
+    ['sts-waiting', defineAsyncComponent(() => import("./StatusWaiting.vue"))],
+    ['sts-reject', defineAsyncComponent(() => import("./StatusReject.vue"))],
+    ['sts-edit', defineAsyncComponent(() => import("./StatusEdit.vue"))],
+    ['sts-sketch', defineAsyncComponent(() => import("./StatusSketch.vue"))],
+]);
 
-onMounted (() => {
-    switch(props.status){
-    //อนุมัติ
-        case "sts-accept" :currentComponent.value =StatusAccept;
-        break;
-    //รออนุมัติ
-        case "sts-waiting" :currentComponent.value =StatusWaiting;
-        break;
-    //ไม่อนุมัติ
-        case "sts-reject" :currentComponent.value =StatusReject;
-        break;
-    //แก้ไข
-        case "sts-edit" :currentComponent.value =StatusEdit;
-        break;
-    //แก้ไข
-        case "sts-sketch" :currentComponent.value =StatusSketch;
-        break;
-    }
-}) 
+onMounted(() => {
+    currentComponent.value = componentMap.get(props.status) || null;
+}); 
 </script>
 
 <template>
-    <div class="flex flex-col items-center text-center ">
-
-        <!-- content -->
-        <div
-            class="flex gap-4 justify-center items-center px-2 text-xs leading-snug text-center text-white whitespace-nowrap bg-emerald-500 rounded-3xl">
-            <component :is="currentComponent"></component>
-        </div>
-        <!-- content -->
-    </div>
+    <component :is="currentComponent"></component>
 </template>
