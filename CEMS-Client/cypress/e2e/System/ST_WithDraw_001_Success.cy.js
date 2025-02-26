f2d37c7bfe3eac7ecffda128036f805672a4659d
0GaMeNaJa0 <herogane13748@gmail.com>
2025-02-26 09:13:55 +0700

    test : finish test case (name is in file)

--- CEMS-Client/cypress/e2e/System/ST_WithDraw_001_Success.cy.js
+++ CEMS-Client/cypress/e2e/System/ST_WithDraw_001_Success.cy.js
@@ -4,6 +4,10 @@ describe(('ทดสอบกระบวนการการนำจ่าย
     });
 
     it('ST_WithDraw_001_Success', () => {
-
+        cy.get(':nth-child(5) > .relative > .flex').click();
+        cy.get('[href="/payment/list"] > .relative > .absolute').click();
+        cy.xpath('//*[@id="app"]/div/div/div/div/div[2]/div/div[2]/div[2]/table/tbody/tr/th[7]/span/div/div/div').click();
+        cy.get('.btn-นำจ่าย').click();
+        cy.get('.btn-ยืนยัน').click();
     });
 });
