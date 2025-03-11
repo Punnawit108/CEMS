49513ede75844d6a0d788ccceede35e47bffe1c8
Nakareeya <65160338@go.buu.ac.th>
2025-03-11 09:41:17 +0700

    fix(CIconSystemSettingsDisbursementTypeExpense,ExpenseManage): แก้ไขสี และไอคอนของหน้าจัดการประเภทค่าใช้จ่าย

--- CEMS-Client/src/components/Icon/CIconSystemSettingsDisbursementTypeExpense.vue
+++ CEMS-Client/src/components/Icon/CIconSystemSettingsDisbursementTypeExpense.vue
@@ -5,10 +5,10 @@
 
 <template>
     <div>
-        <svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
-            width="24" height="24" fill="none" viewBox="0 0 24 24">
-            <path stroke="red" stroke-linecap="round" stroke-width="2"
-                d="M8 7V6a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1h-1M3 18v-7a1 1 0 0 1 1-1h11a1 1 0 0 1 1 1v7a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1Zm8-3.5a1.5 1.5 0 1 1-3 0 1.5 1.5 0 0 1 3 0Z" />
+        <svg :class="`w-[${size}px] h-[${size}px] text-gray-800 dark:text-white`" aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="red" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                        d="M17 8H5m12 0a1 1 0 0 1 1 1v2.6M17 8l-4-4M5 8a1 1 0 0 0-1 1v10a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-2.6M5 8l4-4 4 4m6 4h-4a2 2 0 1 0 0 4h4a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1Z" />
         </svg>
     </div>
 </template>
