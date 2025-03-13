7dfb55c2a7ee5d7d77763e183b267807e40081ae
SatawatFill <65160356@go.buu.ac.th>
2025-03-13 20:00:06 +0700

    fix:แก้ไขชื่อเทสe2e

---
+++ CEMS-Client/cypress/e2e/E2E/TS_E2E_ApprovalList_005/TS_E2E_004.cy.js
@@ -0,0 +1,40 @@
+/*
+* ชื่อไฟล์: TC05_1_1_005-04.cy
+* คำอธิบาย: ส่งกลับคำขอเบิก
+* ชื่อผู้เขียน/แก้ไข: นายศตวรรษ ไตรธิเลน
+* วันที่จัดทำ/แก้ไข:  
+*/
+describe("จัดการการอนุมัติคำขอเบิก", () => {
+    beforeEach(() => {
+        cy.login("65160093", "admin");
+    });
+
+    it('หลังจากเข้าสู่ระบบ', () => {
+        // เลือกเมนู "การเบิกจ่าย" จาก Sidebar
+        cy.xpath('//*[@id="app"]/div/div/div/nav/ul/li[4]/button/div[1]').click();
+        cy.wait(1000);
+
+        // เลือก "รายการเบิกค่าใช้จ่าย" จาก dropdown
+        cy.xpath('//*[@id="app"]/div/div/div/nav/ul/li[4]/ul/li[1]/a/a/div[1]').click();
+
+        // ตรวจสอบว่า URL เปลี่ยนไปตามที่คาดไว้
+        cy.url().should('include', '/approval/list');
+
+        cy.wait(1000);
+
+        cy.xpath('//*[@id="app"]/div/div/div/div/div[2]/div/div[2]/table[11]/tbody/tr/th[8]').click();
+        cy.wait(1000);
+
+
+        cy.xpath('//*[@id="app"]/div/div/div/div/div[2]/div/div[1]/div/button[2]').click();
+
+        cy.xpath('//*[@id="rqReason"]').type('cypress test');
+
+        cy.xpath('//*[@id="app"]/div/div/div/div/div[2]/div[2]/div/div/div[2]/button[2]').click();
+
+        cy.wait(1000);
+
+
+
+    });
+});
