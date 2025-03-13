3946da14370626b8ec53f9eda87df8a175e14e87
thianchai20 <152171290+thianchai20@users.noreply.github.com>
2025-03-13 19:50:48 +0700

    fix(TS02,03,04,16) : จัดทำ Test E2E

---
+++ CEMS-Client/cypress/e2e/E2E/TS_E2E_003/TC_E2E_listWithdraw_003_003_Success.cy.js
@@ -0,0 +1,37 @@
+/*
+* ชื่อไฟล์: TC_E2E_listWithdraw_003_003_Success.cy.js
+* คำอธิบาย: E2e ดูรายละเอียดคำขอเบิกค่าใช้จ่าย ที่มีสถานะ "รออนุมัติ" ส่งออก Success
+* ชื่อผู้เขียน/แก้ไข: นายเทียนชัย คูเมือง
+* วันที่จัดทำ/แก้ไข: 13 มีนาคม 2568
+*/
+describe("ManageExpeneFormTest", () => {
+    beforeEach(() => {
+        cy.login("65160221", "admin");
+    });
+
+    it('หลังจากเข้าสู่ระบบ', () => {
+        // เลือกเมนู "การเบิกจ่าย" จาก Sidebar
+        cy.xpath('//*[@id="app"]/div/div/div/nav/ul/li[2]/button/div[1]').click();
+        cy.wait(1000);
+        // ตรวจสอบว่า dropdown แสดงผล
+        cy.xpath("//ul[contains(@class, 'ml-8 mt-2')]").should('be.visible');
+        // เลือก "รายการเบิกค่าใช้จ่าย" จาก dropdown
+        cy.xpath('//*[@id="app"]/div/div/div/nav/ul/li[2]/ul/li[1]/a/a/div[1]').click();
+
+        // ตรวจสอบว่า URL เปลี่ยนไปตามที่คาดไว้
+        cy.url().should('include', '/disbursement/listWithdraw');
+        // คลิกไอคอน จัดการ ของคนแรก
+        cy.xpath('//*[@id="app"]/div/div/div/div/div[2]/div/div[3]/table[11]/tbody/tr[1]/th[8]/span/div').click();
+        // ตรวจสอบว่า URL เปลี่ยนไปตามที่คาดไว้
+        cy.url().should('include', '/disbursement/listWithdraw/detail/');
+        // ตรวจสอบว่าสถานะเป็น "รออนุมัติ" หรือไม่
+        cy.xpath('//*[@id="app"]/div/div/div/div/div[2]/div/div/div[1]/div[1]/h3/span')
+            .should('have.text', 'รออนุมัติ'); 
+        // กดปุ่ม "ส่งออก"
+        cy.xpath('//*[@id="btn-พิมพ์"]').click();
+        // กดปุ่ม "ยืนยัน"
+        cy.get('.space-x-4 > .btn-ยืนยัน').click();
+        cy.wait(1000);
+
+    });
+});
