cb2508679a193c7ef1b46138fd34828ebd14abfd
65160104 <152173850+65160104@users.noreply.github.com>
2025-03-17 16:30:05 +0700

    fix(Status) :แก้ไขขนาด

--- CEMS-Client/src/components/Status/StatusAccept.vue
+++ CEMS-Client/src/components/Status/StatusAccept.vue
@@ -1,4 +1,7 @@
 <script setup lang="ts"></script>
 <template>
-    <span class="!text-white rounded-[28px] bg-[#12B669] w-[82px] h-[24px] flex justify-center items-center font-sarabun">อนุมัติ</span>
+  <span
+    class="!text-white rounded-[28px] bg-[#12B669] w-fit px-4 h-[24px] flex justify-center items-center font-sarabun"
+    >อนุมัติ</span
+  >
 </template>
