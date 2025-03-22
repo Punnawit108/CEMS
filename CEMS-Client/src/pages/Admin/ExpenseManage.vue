26de764b27c6eb08dd90e389c70a05dae6dda404
armktn <65160093@go.buu.ac.th>
2025-03-22 20:38:13 +0700

    fix: change icon

--- CEMS-Client/src/pages/Admin/ExpenseManage.vue
+++ CEMS-Client/src/pages/Admin/ExpenseManage.vue
@@ -715,8 +715,8 @@ const confirmDeleteExpense = async () => {
         <div class="w-[30%] flex justify-end px-3">
           <p :class="{ 'text-gray-400': item.vhVisible == 0 }" class="text-sm">
             {{ item.payRate ? item.payRate.toLocaleString("en-US", {
-              minimumFractionDigits: 2,
-              maximumFractionDigits: 2
+            minimumFractionDigits: 2,
+            maximumFractionDigits: 2
             }) : '0.00' }}
           </p>
         </div>
@@ -730,8 +730,23 @@ const confirmDeleteExpense = async () => {
             <Icon icon="bin" class="text-gray-600 hover:text-gray-800" />
           </button>
           <button @click="toggleGray(item.id)">
-            <Icon icon="eye" v-if="item.vhVisible == 1" class="text-gray-600 hover:text-gray-800" />
-            <Icon icon="eye-off" v-else class="text-gray-400 hover:text-gray-600" />
+            <div class="flex items-center space-x-1">
+              <template v-if="item.vhVisible == 1">
+                <svg class="w-[24px] h-[24px] text-gray-800 dark:text-white" aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="gray" stroke-width="1.5"
+                    d="M21 12c0 1.2-4.03 6-9 6s-9-4.8-9-6c0-1.2 4.03-6 9-6s9 4.8 9 6Z" />
+                  <path stroke="gray" stroke-width="2" d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+              </template>
+              <template v-else>
+                <svg class="w-[24px] h-[24px] text-gray-800 dark:text-white" aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                  <path stroke="gray" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                    d="M3.933 13.909A4.357 4.357 0 0 1 3 12c0-1 4-6 9-6m7.6 3.8A5.068 5.068 0 0 1 21 12c0 1-3 6-9 6-.314 0-.62-.014-.918-.04M5 19 19 5m-4 7a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+                </svg>
+              </template>
+            </div>
           </button>
         </div>
 
