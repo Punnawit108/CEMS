b087c80748feb9ee85bc717d9889b0ecb5325ff8
thianchai20 <152171290+thianchai20@users.noreply.github.com>
2025-03-22 17:39:06 +0700

    fix(ExpenseManage):แก้ไข Popup ประเภทค่าใช้จ่าย

--- CEMS-Client/src/pages/Admin/ExpenseManage.vue
+++ CEMS-Client/src/pages/Admin/ExpenseManage.vue
@@ -1240,15 +1240,15 @@ const confirmDeleteExpense = async () => {
   <div v-if="isPopupDeleteExpenseOpen"
     class="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
     <div class="bg-white w-[460px] h-[295px] rounded-lg shadow-lg px-6 py-4 flex flex-col justify-center">
-      <div class="flex justify-center mb-4">
-        <svg :class="`w-[72px] h-[72px] text-gray-800 dark:text-white`" aria-hidden="true"
+      <div class="flex justify-center mb-2">
+        <svg :class="`w-[90px] h-[90px] text-gray-800 dark:text-white`" aria-hidden="true"
           xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#FFBE40" viewBox="0 0 24 24">
           <path fill-rule="evenodd"
             d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4a1 1 0 1 0-2 0v5a1 1 0 1 0 2 0V8Zm-1 7a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2H12Z"
             clip-rule="evenodd" />
         </svg>
       </div>
-      <h2 class="text-[24px] font-bold text-center text-black mb-4">
+      <h2 class="text-[24px] font-bold text-center text-black mb-2">
         ยืนยันการลบประเภทค่าใช้จ่าย
       </h2>
       <h2 class="text-[18px] text-center text-[#7E7E7E] mb-4">
