c0ef5f0123e976ba0445b86a0f1e773bd1395bb9
65160210 <65160210@go.buu.ac.th>
2025-03-26 12:58:10 +0700

    style:เพิ้มcomment component statusทั้งหมด

--- CEMS-Client/src/components/Status/StatusSketch.vue
+++ CEMS-Client/src/components/Status/StatusSketch.vue
@@ -1,4 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+/*
+* ชื่อไฟล์: StatusSketch.vue
+* คำอธิบาย: ไฟล์นี้เป็นคอมโพเนนต์สำหรับแสดงตราสถานะแบบร่าง
+* ชื่อผู้เขียน/แก้ไข: นายขุนแผน ไชยโชติ
+* วันที่จัดทำ/แก้ไข: 11 พฤศจิกายน 2567
+*/
+</script>
 <template>
     <span class="!text-white rounded-[28px] bg-[#B6B7BA] w-fit px-4 h-[24px] flex justify-center items-center font-sarabun">แบบร่าง</span>
 </template>   
--- CEMS-Client/src/components/Status/StatusWaiting.vue
+++ CEMS-Client/src/components/Status/StatusWaiting.vue
@@ -1,4 +1,11 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+/*
+* ชื่อไฟล์: StatusWaiting.vue
+* คำอธิบาย: ไฟล์นี้เป็นคอมโพเนนต์สำหรับแสดงตราสถานะรออนุมัติ
+* ชื่อผู้เขียน/แก้ไข: นายขุนแผน ไชยโชติ
+* วันที่จัดทำ/แก้ไข: 11 พฤศจิกายน 2567
+*/
+</script>
 <template>
     <span class="!text-white rounded-[28px] bg-[#1976D2] w-fit px-4 h-[24px] flex justify-center items-center font-sarabun">รออนุมัติ</span>
 </template>
