74d07d79d54d84904e1348dac3670e9924c7ea2b
armktn <65160093@go.buu.ac.th>
2025-03-26 16:43:42 +0700

    docs: update comments

--- CEMS-Client/src/components/Status/StatusEdit.vue
+++ CEMS-Client/src/components/Status/StatusEdit.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 /*
-* ชื่อไฟล์: StatusEdit.vue
-* คำอธิบาย: ไฟล์นี้เป็นคอมโพเนนต์สำหรับแสดงตราสถานะแก้ไข
-* ชื่อผู้เขียน/แก้ไข: นายขุนแผน ไชยโชติ
-* วันที่จัดทำ/แก้ไข: 11 พฤศจิกายน 2567
-*/
+ * ชื่อไฟล์: StatusEdit.vue
+ * คำอธิบาย: ไฟล์นี้เป็นคอมโพเนนต์สำหรับแสดงตราสถานะแก้ไข
+ * ชื่อผู้เขียน/แก้ไข: นายขุนแผน ไชยโชติ
+ * วันที่จัดทำ/แก้ไข: 11 พฤศจิกายน 2567
+ */
 </script>
 <template>
-    <span class="!text-white rounded-[28px] bg-[#FFBE40] w-fit px-4 h-[24px] flex justify-center items-center font-sarabun">แก้ไข</span>
-</template>   
+  <span
+    class="!text-white rounded-[28px] bg-[#FFBE40] w-fit px-4 h-[24px] flex justify-center items-center font-sarabun"
+    >แก้ไข</span
+  >
+</template>
--- CEMS-Client/src/components/Status/StatusWaiting.vue
+++ CEMS-Client/src/components/Status/StatusWaiting.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 /*
-* ชื่อไฟล์: StatusWaiting.vue
-* คำอธิบาย: ไฟล์นี้เป็นคอมโพเนนต์สำหรับแสดงตราสถานะรออนุมัติ
-* ชื่อผู้เขียน/แก้ไข: นายขุนแผน ไชยโชติ
-* วันที่จัดทำ/แก้ไข: 11 พฤศจิกายน 2567
-*/
+ * ชื่อไฟล์: StatusWaiting.vue
+ * คำอธิบาย: ไฟล์นี้เป็นคอมโพเนนต์สำหรับแสดงตราสถานะรออนุมัติ
+ * ชื่อผู้เขียน/แก้ไข: นายขุนแผน ไชยโชติ
+ * วันที่จัดทำ/แก้ไข: 11 พฤศจิกายน 2567
+ */
 </script>
 <template>
-    <span class="!text-white rounded-[28px] bg-[#1976D2] w-fit px-4 h-[24px] flex justify-center items-center font-sarabun">รออนุมัติ</span>
+  <span
+    class="!text-white rounded-[28px] bg-[#1976D2] w-fit px-4 h-[24px] flex justify-center items-center font-sarabun"
+    >รออนุมัติ</span
+  >
 </template>
